d8f0bd7d32f4fb1aeaeb30a1a976ba3d60afb19c
Lucas Pinto <fc56926@alunos.fc.ul.pt>
2023-02-23 23:24:38 +0000

    feat: add minion

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import bobImage from '../assets/bob.jpg';
+</script>
+
+<template>
+    <div id="container">
+        <div id="subcontainer">
+            <h1>Minions</h1>
+            <img :src="bobImage" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+}
+#subcontainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+img {
+  width: 100%;
+  max-width: max(70%, 700px);
+  border-radius: 10px;
+}
+</style>
